69b99ad746d8ddb03b2976441fc152fd8d30d0de
Christine Nguyen <cnhnguyen59@gmail.com>
2022-01-22 01:32:31 -0500

    created get and post routes for new notes

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs')
+const notesLog = require('./db/db.json')
 
-const PORT = 3001;
+let rawData = fs.readFileSync('./db/db.json')
+let notesArr = JSON.parse(rawData)
+console.log(notesArr)
+
+const PORT = 4000;
 
 const app = express();
 
@@ -10,16 +16,50 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
+// Routes
 app.get('/', (req, res) =>{
     res.sendFile(path.join(--dirname, '/public/index.html'))
 })
 
-///////
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/notes.html'))
+);
 
+//GET requets for notes
+app.get('/api/notes', (req, res) =>
+  res.json(notesLog)
+  
+);
 
+//POST requets for notes
+app.post('/api/notes', (req, res)=>{
 
+    const {title, text} = req.body;
 
-///////
+    if(title && text){
+        const newNote = {
+            title,
+            text,
+            id: 1
+        }
+        notesArr.push(newNote)
+        console.log(notesArr)
+        console.log(typeof notesArr)
+
+        let noteString = JSON.stringify(notesArr)
+
+        fs.writeFile('./db/db.json', noteString, err => {
+            err 
+            ? console.err(err)
+            : res.send('New note added to JSON file')
+     
+         })
+        
+    }
+})
+
+// DELETE request for notes
+ 
 
 app.listen(PORT,()=>
     console.log(`App listening at http://localhost:${PORT}`)
